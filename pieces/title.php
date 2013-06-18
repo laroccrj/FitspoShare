a7b2a5c30d951b35cfd485053be6f5d1875dfabc
Robert La Rocca <laroccrj@alfredstate.edu>
2013-06-18 08:02:56 -0400

    default profile picture setup. Can now see profile info in top right when logged in

--- pieces/title.php
+++ pieces/title.php
@@ -2,17 +2,25 @@
     <div id="title">
         Fitspo Share
     </div>
-    <?php if(ISSET($_SESSION["user"]) && $_SESSION["user"]->loggedIn){ ?>
-    
-    <?php } else { ?>
-        <div id="login">
-            <a href="signUp.php">Sign Up</a><br />
-            <form action="actions/login.php" method="post">
-                Username: <input type="text" name="username"> 
-                <?php $error->checkError("lgoin", "username"); ?><br />
-                Password: <input type="password" name="password"> <br />
-                <input type="submit">
-            </form>
-        </div>
-    <?php } ?>
+    <div id="access">
+        <?php if(ISSET($_SESSION["user"]) && $_SESSION["user"]->loggedIn){ ?>
+            <div id="info">
+                
+                <div id="img">
+                    <img src="images/profile/<?php echo $_SESSION["user"]->profilePicture; ?>" >
+                </div>
+                <?php echo $_SESSION["user"]->nickname; ?>
+            </div>
+        <?php } else { ?>
+            <div id="login">
+                <a href="signUp.php">Sign Up</a><br />
+                <form action="actions/login.php" method="post">
+                    Username: <input type="text" name="username"> 
+                    <?php $error->checkError("lgoin", "username"); ?><br />
+                    Password: <input type="password" name="password"> <br />
+                    <input type="submit">
+                </form>
+            </div>
+        <?php } ?>
+    </div>
 </div>
